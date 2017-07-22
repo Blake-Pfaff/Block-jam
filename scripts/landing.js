777828566d3939991211d711a6b38df3bd177854
Thrilldozer <blake.a.pfaff2.com>
2017-07-22 15:11:26 -0500

    adds notes for js

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,12 +1,12 @@
 
 var animatePoints = function() {
-
+    // turns point into points array
     var points = document.getElementsByClassName('point');
-
+   
     for (let i = 0; i < points.length; i++) {
-            points[i].style.opacity = 1;
-            points[i].style.transform = "scaleX(1) translateY(0)";
-            points[i].style.msTransform = "scaleX(1) translateY(0)";
-            points[i].style.webkitTransform = "scaleX(1) translateY(0)";
+        points[i].style.opacity = 1;
+        points[i].style.transform = "scaleX(1) translateY(0)";
+        points[i].style.msTransform = "scaleX(1) translateY(0)";
+        points[i].style.webkitTransform = "scaleX(1) translateY(0)";
         }
     };
