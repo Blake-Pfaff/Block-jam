5890a09a6471859727f096d5ca5b532bfc722e82
Thrilldozer <blake.a.pfaff2.com>
2017-08-02 14:19:04 -0500

    fist half of checpoint 19

--- scripts/album.js
+++ scripts/album.js
@@ -1,50 +1,5 @@
 
-var albumPicasso = {
-    title: 'The Colors',
-    artist: 'Pablo Picasso',
-    label: 'Cubism',
-    year: '1881',
-     albumArtUrl: 'assets/images/album_covers/01.png',
-     songs: [
-        { title: 'Blue', duration: '4:26'},
-        { title: 'Green', duration: '3:14'},
-        { title: 'Red', duration: '5:01'},
-        { title: 'Pink', duration: '3:21'},
-        { title: 'Magenta', duration: '2:15'},
-    ]
-};
 
-var albumMarconi = {
-    title: 'The Telephone',
-    artist: 'Guglielmo Marconi',
-    label: 'EM',
-    year: '1909',
-    albumArtUrl: 'assets/images/album_covers/20.png',
-    songs: [
-        { title: 'Hello, Operator?', duration: '1:01' },
-        { title: 'Ring, ring, ring', duration: '5:01' },
-        { title: 'Fits in your pocket', duration: '3:21'},
-        { title: 'Can you hear me now?', duration: '3:14' },
-        { title: 'Wrong phone number', duration: '2:15'}
-    ]
-};
-
-var albumNin = {
-     title: 'broken',
-     artist: 'Nine Inch Nails',
-     label: 'Interscope and Nothing',
-     year: '1993',
-     albumArtUrl: 'assets/images/album_covers/nin.jpg',
-     songs: [
-         { title: 'Pinion', duration: '1:02' },
-         { title: 'Wish', duration: '3:46' },
-         { title: 'Help Me I Am In Hell', duration: '1:56'},
-         { title: 'Happiness In Slavery', duration: '5:21' },
-         { title: 'Gave Up', duration: '4:08'},
-         { title: 'Physical', duration: '5:29'},
-        { title: 'Suck', duration: '5:07'}
-      ]
-};
 var createSongRow = function(songNumber, songName, songLength) {
      var template =
          '<tr class="album-view-song-item">'
@@ -56,47 +11,35 @@ var createSongRow = function(songNumber, songName, songLength) {
 
      var $row = $(template);
 
-    var clickHandler = function() {
-      var songItem = getSongItem(targetElement);
-      if (currentlyPlayingSong === null) {
-      songItem.innerHTML = pauseButtonTemplate;
-      currentlyPlayingSong = songItem.getAttribute('data-song-number');
-      } else if (currentlyPlayingSong === songItem.getAttribute('data-song-number')) {
-         songItem.innerHTML = playButtonTemplate;
-         currentlyPlayingSong = null;
-       } else if (currentlyPlayingSong !== songItem.getAttribute('data-song-number')) {
-           var currentlyPlayingSongElement = document.querySelector('[data-song-number="' + currentlyPlayingSong + '"]');
-           currentlyPlayingSongElement.innerHTML = currentlyPlayingSongElement.getAttribute('data-song-number');
-           songItem.innerHTML = pauseButtonTemplate;
-           currentlyPlayingSong = songItem.getAttribute('data-song-number');
-       }
-    };
 
      var onHover = function(event) {
          var songNumberCell = $(this).find('.song-item-number');
          var songNumber = songNumberCell.attr('data-song-number');
 
-         if (songNumber !== currentlyPlayingSong) {
+         if (songNumber !== currentlyPlayingSongNumber) {
              songNumberCell.html(playButtonTemplate);
          }
      };
 
     var clickHandler = function() {
-	  var songNumber = $(this).attr('data-song-number');
 
-	  if (currentlyPlayingSong !== null) {
+    var songNumber = $(this).attr('data-song-number');
+
+	  if (currentlyPlayingSongNumber !== null) {
 		// Revert to song number for currently playing song because user started playing new song.
-		var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSong + '"]');
-		  currentlyPlayingCell.html(currentlyPlayingSong);
+		var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
+		  currentlyPlayingCell.html(currentlyPlayingSongNumber);
 	  }
-	  if (currentlyPlayingSong !== songNumber) {
+	  if (currentlyPlayingSongNumber !== songNumber) {
 		// Switch from Play -> Pause button to indicate new song is playing.
 		$(this).html(pauseButtonTemplate);
-		currentlyPlayingSong = songNumber;
-	  } else if (currentlyPlayingSong === songNumber) {
+		currentlyPlayingSongNumber = songNumber;
+    currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+  } else if (currentlyPlayingSongNumber === songNumber) {
 		// Switch from Pause -> Play button to pause currently playing song.
 		$(this).html(playButtonTemplate);
-		currentlyPlayingSong = null;
+		currentlyPlayingSongNumber = null;
+    currentSongFromAlbum = null;
 	}
 };
 
@@ -104,7 +47,7 @@ var createSongRow = function(songNumber, songName, songLength) {
          var songNumberCell = $(this).find('.song-item-number');
          var songNumber = songNumberCell.attr('data-song-number');
 
-         if (songNumber !== currentlyPlayingSong) {
+         if (songNumber !== currentlyPlayingSongNumber) {
              songNumberCell.html(songNumber);
          }
      };
@@ -125,6 +68,7 @@ var albumImage = document.getElementsByClassName('album-cover-art')[0];
 var albumSongList = document.getElementsByClassName('album-view-song-list')[0]
 
 var setCurrentAlbum = function(album) {
+  currentAlbum = album;
     // gets elements needed to make the diaplay
   var $albumTitle = $('.album-view-title');
   var $albumArtist = $('.album-view-artist');
@@ -153,11 +97,10 @@ var playButtonTemplate =  '<a class="album-song-button"><span class="ion-play"><
 var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
 // store state od playing songs
 var currentlyPlayingSong = null;
+var currentlyPlayingSongNumber = null;
 
 $(document).ready(function() {
     setCurrentAlbum(albumPicasso);
-
-
 
   });
 
---
+++ scripts/fixtures.js
@@ -0,0 +1,47 @@
+
+var albumPicasso = {
+    title: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+        { title: 'Blue', duration: '4:26'},
+        { title: 'Green', duration: '3:14'},
+        { title: 'Red', duration: '5:01'},
+        { title: 'Pink', duration: '3:21'},
+        { title: 'Magenta', duration: '2:15'},
+    ]
+};
+
+var albumMarconi = {
+    title: 'The Telephone',
+    artist: 'Guglielmo Marconi',
+    label: 'EM',
+    year: '1909',
+    albumArtUrl: 'assets/images/album_covers/20.png',
+    songs: [
+        { title: 'Hello, Operator?', duration: '1:01' },
+        { title: 'Ring, ring, ring', duration: '5:01' },
+        { title: 'Fits in your pocket', duration: '3:21'},
+        { title: 'Can you hear me now?', duration: '3:14' },
+        { title: 'Wrong phone number', duration: '2:15'}
+    ]
+};
+
+var albumNin = {
+     title: 'broken',
+     artist: 'Nine Inch Nails',
+     label: 'Interscope and Nothing',
+     year: '1993',
+     albumArtUrl: 'assets/images/album_covers/nin.jpg',
+     songs: [
+         { title: 'Pinion', duration: '1:02' },
+         { title: 'Wish', duration: '3:46' },
+         { title: 'Help Me I Am In Hell', duration: '1:56'},
+         { title: 'Happiness In Slavery', duration: '5:21' },
+         { title: 'Gave Up', duration: '4:08'},
+         { title: 'Physical', duration: '5:29'},
+        { title: 'Suck', duration: '5:07'}
+      ]
+};
